8b5cd8bb72ca076805c530601707023dab59e0e1
Paul Otten <lightnica@yahoo.ca>
2024-12-02 14:38:15 -0500

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -24,25 +24,63 @@ fn part1() {
     println!("{count_safe}")
 }
 
-fn part2() {
-    println!("Part 2");
-    println!("TODO");
-}
-
 fn report_is_safe(report: &Vec<i32>) -> bool {
     assert!(report.len() >= 2);
 
     let increasing = report[0] < report[1];
 
+    report_is_safe_inner(&report, increasing, true)
+}
+
+fn part2() {
+    println!("Part 2");
+
+    let input = get_input();
+    let reports = parse_input(input);
+
+    let mut count_safe = 0;
+
+    for report in reports {
+        if report_is_safe2(&report) {
+            count_safe += 1;
+        }
+    }
+
+    println!("{count_safe}")
+}
+
+fn report_is_safe2(report: &Vec<i32>) -> bool {
+    // skipping the first or second level could change if the report is increasing or decreasing
+    // so try both
+    report_is_safe_inner(&report, true, false) || report_is_safe_inner(&report, false, false)
+}
+
+fn report_is_safe_inner(report: &[i32], increasing: bool, skipped: bool) -> bool {
     for i in 1..report.len() {
         let (smaller, larger) = if increasing {
-            (report[i-1], report[i])
+            (report[i - 1], report[i])
         } else {
-            (report[i], report[i-1])
+            (report[i], report[i - 1])
         };
 
         if larger < smaller + 1 || larger > smaller + 3 {
-            return false
+            if skipped {
+                return false;
+            } else {
+                // try removing current level
+                let mut copy = report.to_vec();
+                copy.remove(i);
+
+                if report_is_safe_inner(&copy, increasing, true) {
+                    return true;
+                }
+
+                // try removing previous level
+                let mut copy = report.to_vec();
+                copy.remove(i - 1);
+
+                return report_is_safe_inner(&copy, increasing, true);
+            }
         }
     }
 
